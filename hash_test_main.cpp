d79e1af4c4d3100b168bc9f24a40a77eec54f3a3
PraiseTheGoldenHoodie <34354911+jose01carrillo30@users.noreply.github.com>
2019-10-28 01:26:02 +0100

    created WIP scaffolding for hashing, including printBoard()

---
+++ hash_test_main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+typedef uint64_t ULL; // ensures ULL is 64 bits
+
+enum : unsigned char {WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK};
+
+
+class Board {
+    public:
+        ULL bitboards[12]; //2 colors each with 6 types of pieces
+        bool CWK, CWQ, CBK, CBQ; // castle rights
+        short kings; // king locations: kings = WK*64 + BK
+        short EP; // en passant column / position?
+        short materialWhite, materialBlack; // material scores for white and black
+        short movesSinceLastCapture; // 50 move rule
+        ULL hashCode; //TODO: should this really be a field of Board?
+};
+
+
+//TODO: there has to be a better way than this:
+const string pieceRepresentation = "PNBRQKpnbrqk";
+
+void printBoard(Board* board) {
+    /**TODO: outputs nice visual given board*/
+    cout << "Board: " << board->hashCode << endl;
+
+    //pring bitboards
+    /* //Useful for debuggging 
+    for (int pieceType = 0; pieceType < 12; pieceType++)
+    { 
+        cout << pieceRepresentation[pieceType] << " : " << board->bitboards[pieceType] << endl; 
+    } */
+
+    // print grid
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            char toPrint = '.';
+            for (int pieceType = 0; pieceType < 12; pieceType++)
+            {
+                //cout << "mask for "<<i<<", "<<j<<" : " << ((ULL(1)) << (8*i + j)) << endl;
+                if(board->bitboards[pieceType] & ( ULL(1) << (8*i + j)))
+                    toPrint = pieceRepresentation[pieceType];
+            }
+            cout << toPrint;
+        }
+        cout << endl;
+    }
+}
+
+Board* createFromFenn(string fenn) {
+    /** TODO: */
+}
+
+#define NUM_TEST_BOARDS 5
+Board** createSomeBoards() {
+    /** Method to generate boards to be tested */
+    Board** boards = new Board*[NUM_TEST_BOARDS];
+    for(int i = 0; i < NUM_TEST_BOARDS; i++)
+    {
+        boards[i] = new Board();
+        boards[i]->hashCode = i*i;
+    }
+    return boards;
+}
+
+ULL createHash(Board* board) {
+    /** TODO: Hashes 84 bits of board info into 64 bits */
+    return board->bitboards[WP]; //TODO: temporary. test by literally returing the white pawn layout as the hash
+}
+
+int main() {
+
+    //TODO: replace this with the FEN string
+    Board STARTING_BOARD = Board();
+    {
+    STARTING_BOARD.CWK = true;
+    STARTING_BOARD.CWQ = true;
+    STARTING_BOARD.CBK = true;
+    STARTING_BOARD.CBQ = true;
+    STARTING_BOARD.bitboards[WP] = 0xff00;
+    STARTING_BOARD.bitboards[BP] = 0xff000000000000;
+    STARTING_BOARD.bitboards[WR] = 0x81;
+    STARTING_BOARD.bitboards[BR] = 0x8100000000000000;
+    STARTING_BOARD.bitboards[WN] = 0x42;
+    STARTING_BOARD.bitboards[BN] = 0x4200000000000000;
+    STARTING_BOARD.bitboards[WB] = 0x24;
+    STARTING_BOARD.bitboards[BB] = 0x2400000000000000;
+    STARTING_BOARD.bitboards[WQ] = 0x8;
+    STARTING_BOARD.bitboards[BQ] = 0x800000000000000;
+    STARTING_BOARD.bitboards[WK] = 0x10;
+    STARTING_BOARD.bitboards[BK] = 0x1000000000000000;
+    STARTING_BOARD.CWK = true;
+    STARTING_BOARD.CWQ = true;
+    STARTING_BOARD.CBK = true;
+    STARTING_BOARD.CBQ = true;
+    STARTING_BOARD.kings = 64*3 + 59; // king locations: kings = WK*64 + BK // doesn't matter for hashing
+    STARTING_BOARD.EP = 0; // Assuming 0 means no en passant
+    // materialWhite, materialBlack; // doesn't matter for hashing
+    STARTING_BOARD.movesSinceLastCapture = 0; // I don't think this matters for hashing really, only really useful when close to 50 
+    STARTING_BOARD.hashCode = createHash(&STARTING_BOARD);
+    }
+
+    cout << "****************" << endl;
+    cout << "Testing program:" << endl;
+    cout << "****************" << endl;
+
+    printBoard(&STARTING_BOARD);
+
+    Board** testBoards = createSomeBoards();
+
+    // print all boards
+    for(int i=0; i < NUM_TEST_BOARDS; i++) {
+        printBoard(testBoards[i]);
+    }
+
+
+}
+
