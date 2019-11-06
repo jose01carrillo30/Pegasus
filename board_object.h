ec80508e30916874e8f13a640af4ddf1725c994d
Troy Lee <troylee3006@tamu.edu>
2019-11-06 15:44:35 -0600

    Reorganized code so that typedef of ULL and the board object are in a single header file.

---
+++ board_object.h
@@ -0,0 +1,24 @@
+//
+// Created by troyl on 11/6/2019.
+//
+
+#ifndef PEGASUS_BOARD_OBJECT_H
+#define PEGASUS_BOARD_OBJECT_H
+
+#include <cstdint>
+
+typedef uint64_t ULL; // ensures ULL is 64 bits
+
+enum : unsigned char {WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK};
+
+struct Board {
+    ULL bitboards[12];
+    bool CWK, CWQ, CBK, CBQ; // castle rights
+    short kings; // king locations: kings = WK*64 + BK
+    short EP; // en passant column / position?
+    short materialWhite, materialBlack; // material scores for white and black
+    short movesSinceLastCapture; // 50 move rule
+    ULL hashCode;
+};
+
+#endif //PEGASUS_BOARD_OBJECT_H
