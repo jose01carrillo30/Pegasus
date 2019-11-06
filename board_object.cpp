33c7087d00c53a740e82aa1d4179051d259b0e3f
Troy Lee <troylee3006@tamu.edu>
2019-11-06 15:50:11 -0600

    test to see if I can modify existing files

--- board_object.cpp
+++ board_object.cpp
@@ -1,13 +1,13 @@
-typedef unsigned long long ULL;
-
-enum : unsigned char {WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK};
-
-struct Board {
-    ULL bitboards[12];
-    bool CWK, CWQ, CBK, CBQ; // castle rights
-    short kings; // king locations: kings = WK*64 + BK
-    short EP; // en passant column / position?
-    short materialWhite, materialBlack; // material scores for white and black
-    short movesSinceLastCapture; // 50 move rule
-    ULL hashCode;
-};
+//typedef unsigned long long ULL;
+//
+//enum : unsigned char {WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK};
+//
+//struct Board {
+//    ULL bitboards[12];
+//    bool CWK, CWQ, CBK, CBQ; // castle rights
+//    short kings; // king locations: kings = WK*64 + BK
+//    short EP; // en passant column / position?
+//    short materialWhite, materialBlack; // material scores for white and black
+//    short movesSinceLastCapture; // 50 move rule
+//    ULL hashCode;
+//};
