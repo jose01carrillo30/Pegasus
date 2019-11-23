70977adf76c1c08e3842411d785d462909ca1dbf
PraiseTheGoldenHoodie <34354911+jose01carrillo30@users.noreply.github.com>
2019-11-23 04:59:39 +0000

    created preliminary hashing method

--- hash_test_main.cpp
+++ hash_test_main.cpp
@@ -178,21 +178,41 @@ uint64_t randomHash() {
     return distribution(generator);
 }
 uint64_t createHash(board::Board* board) {
-    /** TODO: Hashes into 64 bits */
-    return 420; //TODO: temporary
-    for (size_t r = 0; r < 8; r++) { // Loop over valid spaces
-        size_t R = 10*r; // row offset
-        for (size_t c = 0; c < 8; c++) { 
-            //board->chessboard[21 + R + c]
+    /** Hashes into 64 bits */
+    uint64_t hash = 0;
+    /* Pack all additional info into 16 bit number */
+    hash += board->turnWhite << 0; // +1 bit, total 1
+    hash += board->CBK << 1; // +1 bit, total 2
+    hash += board->CBQ << 2; // +1 bit, total 3
+    hash += board->CWK << 3; // +1 bit, total 4
+    hash += board->CWQ << 4; // +1 bit, total 5
+    hash += board->EP << 5; // +7 bit, total 12 //TODO: what are the values stored in EP? might need to do some transform to it depending on this
+    // Map MSLC to 8 bits as follows: 1-7 for last 7 moves, 0 otherwise
+    char MSLC = 50 + 1 - board->movesSinceLastCapture; //TODO: Am I off by one? Is 50 or 51 the largest value that can be stored into movesSinceLastCapture?
+    if (MSLC > 7) MSLC = 0;  
+    hash += MSLC << 12; // +3 bit, total 15
+
+    /* Hash board positions */
+    /* Break the chessboard data into 4 sections of 64 bits */
+    for (short i = 0; i < 4; i++) { 
+        // Each section is made up of 16 spaces, each space requiring 4 bits
+        uint64_t section = 0;
+        short cursor = 21 + 20*i; // set cursor to start of section. 21 is first space, 20*i is 2*i-th row
+        for (short j = 0; j < 16; j++) {
+            section += board->chessboard[cursor] << (4*j); // add 4 bits of the space at cursor to section, no overlap
+            if (j % 8 == 7) cursor += 2; // skip padding between rows
         }
 
+        /* heart of the hash function here */
+        hash ^= 199*section; //TODO: this is not sufficient by any measure. Find a much better hash function.
     }
+    return hash;
 }
 
 typedef priority_queue<board::Board*, std::vector<board::Board*>, CompareHashes> board_queue;
 
 //#define NUM_TEST_BOARDS 3
-#define NUM_TEST_BOARDS 10000000
+#define NUM_TEST_BOARDS 1000000
 /** Method to generate boards to be tested */
 board_queue createSomeBoards() {
 //void createSomeBoards() {
@@ -214,12 +234,12 @@ board_queue createSomeBoards() {
         for (size_t i = 0; i < pieces_length; i++)
             nextBoard->chessboard[randomPosition()] = pieces[i];
         // Generate hash 
-        //nextBoard->hashCode = createHash(nextBoard); //Change which of these is commented out to select them
-        nextBoard->hashCode = randomHash();
+        nextBoard->hashCode = createHash(nextBoard); //Change which of these is commented out to select them
+        //nextBoard->hashCode = randomHash();
         // store to queue for later
         boards.push(nextBoard);
     }
-    return boards; //TODO:
+    return boards;
 }
 
 //TODO: renamed and run from "actual_main_here.cpp"
@@ -228,20 +248,18 @@ int mainRename_hash_test_main() {
     cout << "Generating " << utility::toCommaString(NUM_TEST_BOARDS) << " random boards..." << endl;
     board_queue testBoards = createSomeBoards();
 
+    //TODO: would be nice if collision counting was more efficient
     cout << "Counting collisions..." << endl;
     int collisions = 0;
     board::Board* prev = nullptr;
     for(int i=0; !testBoards.empty(); i++) {
         if (NUM_TEST_BOARDS < 10) { // only show boards if very small batch
             utility::printBoardArray(testBoards.top());
+            cout << endl;
         }
-        //cout << endl;
+
         if (prev && prev->hashCode == testBoards.top()->hashCode) {
             collisions++;
-            // utility::printBoardArray(testBoards.top());
-            // cout << endl;
-            // utility::printBoardArray(prev);
-            // cout << endl;
         }
         delete prev;
         prev = testBoards.top();
